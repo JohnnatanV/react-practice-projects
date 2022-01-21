ca07cb36e22ee0afa6488ef4e804ab4153e437ff
JohnnatanV <joh.villaneda@outlook.com>
2022-01-21 12:14:41 -0500

    testing git

--- peliculas/src/App.js
+++ peliculas/src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let nombre = "Johnnatan";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hola {nombre} <br />
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
