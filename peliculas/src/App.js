0741644671f4b6f5b27b69a3479466d2a19bc2e0
JohnnatanV <joh.villaneda@outlook.com>
2022-01-23 01:27:36 -0500

    useEffects comming soon

--- peliculas/src/App.js
+++ peliculas/src/App.js
@@ -1,20 +1,50 @@
 import { useState } from "react";
-import "./App.css";
+
 import PageWrapper from "./PageWrapper";
 import Paginacion from "./Paginacion";
 import Pelicula from "./Pelicula";
 import PeliculasJson from "./peliculas.json";
+import "./App.css";
 
 function App() {
   const [paginaActual, setPaginaActual] = useState(1);
 
   let peliculas = PeliculasJson;
-  const TOTAL_POR_PAGINA = 5;
+  const TOTAL_POR_PAGINA = 4;
 
-  peliculas = peliculas.slice(
-    (paginaActual - 1) * TOTAL_POR_PAGINA,
-    paginaActual + TOTAL_POR_PAGINA
-  );
+  let buscarPeliculas = async () => {
+    let url = `https://api.allorigins.win/get?url=${encodeURIComponent(
+      "https://lucasmoy.dev/data/react/peliculas.json"
+    )}`;
+
+    let result = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "aplication/json",
+        "Content-type": "aplication/json",
+      },
+    }); // .then(response => {})
+
+    let content = await result.json();
+    console.log(content.contents);
+  };
+
+  buscarPeliculas();
+
+  const cargarPeliculas = () => {
+    peliculas = peliculas.slice(
+      (paginaActual - 1) * TOTAL_POR_PAGINA,
+      paginaActual * TOTAL_POR_PAGINA
+    );
+  };
+
+  const getTotalPaginas = () => {
+    let totalDePeliculas = PeliculasJson.length;
+    return Math.ceil(totalDePeliculas / TOTAL_POR_PAGINA);
+  };
+
+  cargarPeliculas();
+
   return (
     <PageWrapper>
       {peliculas.map((pelicula) => (
@@ -32,7 +62,7 @@ function App() {
       ))}
       <Paginacion
         pagina={paginaActual}
-        total={4}
+        total={getTotalPaginas()}
         onChange={(pagina) => {
           setPaginaActual(pagina);
         }}
